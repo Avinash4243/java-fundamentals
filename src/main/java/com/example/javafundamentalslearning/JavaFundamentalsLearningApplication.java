db806f0009a564402603a8a4262ef95b6d60a317
AD\avpand <avinash.pandey@assaabloy.com>
2025-10-29 12:03:31 +0530

    initial setup

---
+++ src/main/java/com/example/javafundamentalslearning/JavaFundamentalsLearningApplication.java
@@ -0,0 +1,9 @@
+package com.example.javafundamentalslearning;
+
+
+public class JavaFundamentalsLearningApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Working...");
+    }
+}
